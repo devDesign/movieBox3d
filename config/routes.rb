d53329b6e5abf1a1d89c77603aa004b6b9100c9f
devDesign <marlyguha@gmail.com>
2014-10-01 18:24:30 -0700

    session resource and movie partial

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ resources :movies do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
